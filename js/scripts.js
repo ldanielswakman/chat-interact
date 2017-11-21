9704231521016e57661a854b1b40f649c4ee5a93
L Daniel Swakman <d.swakman@gmail.com>
2017-11-21 13:42:10 +0100

    building some logic etc

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,34 @@
-// Contact Form Dialog
+
+// ----------------- //
+//
+// Chat interact JS
+//
+// ----------------- //
+// Requirements:
+// - jQuery
+// - Typed.js
+
 $(document).ready(function() {
 
   // init
+  init( $('.chat-interact') );
   chatInteract( $('.chat-interact') );
 
 });
+
+
+function init($target) {
+  loader = '<div class="bubble__loader"><img src="images/spinner-new.svg" alt="" style="height: 1rem;" /></div>';
+  $target.find('.bubble-wrap').each(function(i) {
+    // make bubbles referenceable
+    $(this).attr('id', 'bubble' + (i+1));
+    // add loader for non-user bubbles
+    if(!$(this).hasClass('bubble-wrap--right')) {
+      $(this).find('.bubble').prepend(loader);
+    }
+  });
+}
+
 
 function chatInteract($target) {
 
@@ -12,16 +36,30 @@ function chatInteract($target) {
   bubbleRun( $target.find('.bubble-wrap#bubble1') );
 
   // Continue button
-  $target.find('[data-action="continue"]').on('click touchstart', function (e) {
+  $target.find('[data-action]').on('click touchstart', function (e) {
     e.preventDefault();
-    if ($(this).closest('.bubble-wrap.isLoaded').nextAll().length > 0) {
-      $next = $(this).closest('.bubble-wrap');
-      $(this).closest('.bubble-wrap').nextAll().removeClass('isLoaded');
-      setTimeout(function() { bubbleRun($next) }, 500);
-    } else {
-      bubbleRun($(this).closest('.bubble-wrap'));
+
+    action = $(this).attr('data-action');
+
+    if(action == 'continue') {
+      if ($(this).closest('.bubble-wrap.isLoaded').nextAll().length > 0) {
+        $next = $(this).closest('.bubble-wrap');
+        $(this).closest('.bubble-wrap').nextAll().removeClass('isLoaded');
+        setTimeout(function() { bubbleRun($next) }, 500);
+      } else {
+        bubbleRun($(this).closest('.bubble-wrap'));
+      }
+    } else if($('#' + action).length == 1) {
+      bubbleRun($('#' + action));
+    // error cases
+    } else if($('#' + action).length > 1) {
+      console.log("Action error: more than one instance found.");
+    } else if($('#' + action).length == 0) {
+      console.log("Action error: no instance found.");
     }
   });
+
+
   // Continue key combination (Shift + Enter)
   $target.find('.bubble').find('textarea, input').on('keypress keydown', function(e) {
     if(e.keyCode == 13 && e.shiftKey == true) {
@@ -63,17 +101,34 @@ function chatInteract($target) {
 // }
 
 // 'Run' Bubble
-function bubbleRun($obj, stophere) {
+function bubbleRun($obj, stophere, nextTarget) {
   nextTimeout = 0;
+  nextTarget = (nextTarget) ? nextTarget : null;
+  if(nextTarget === null && $(this).attr('data-next')) {
+    nextTarget = $(this).attr('data-next');
+  }
   $form = $obj.closest('form');
 
   if(!$obj.hasClass('isLoaded')) {
     $obj.addClass('isLoaded');
+
+    // TODO: jQuery 'pause' run instead of setTimeout?
+
+    setTimeout(function() {
+      $obj.find('.typed-target').typed({
+        stringsElement: $obj.find('.typed-content'),
+        typeSpeed: 3,
+        callback: function() {
+          $obj.find('.typed-cursor').addClass('isHidden');
+        },
+      });
+    }, 600);
+
     if($form[0]) {
       $form.scrollTop($form[0].scrollHeight);
     }
     $obj.find('textarea, input').first().focus();
-    nextTimeout = 1500;
+    nextTimeout = 2000;
   }
 
   if($obj.attr('data-autoplay') == 'false') {
